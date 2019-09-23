e51c9eac83a66b1967acd05e0141429bcc6c7bc6
thechallenger346 <45742653+thechallenger346@users.noreply.github.com>
2019-09-22 21:18:34 -0400

    Add files via upload

---
+++ virus_scanner.py
@@ -0,0 +1,7 @@
+#version 1.0.0.0
+#made by the challenger 346
+print("What virus are you scanning for?")
+a = input()
+if a == a: #checks to see if the input will equal the input
+    while 3 > 2: #makes the lines below repeat
+        print("The " + a + " virus has been found!")
