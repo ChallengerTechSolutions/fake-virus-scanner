0d2e01b700fbf1a787df6c605e4a2aff8f925785
thechallenger346 <45742653+thechallenger346@users.noreply.github.com>
2019-09-23 18:45:03 -0400

    Add files via upload

--- virus_scanner.py
+++ virus_scanner.py
@@ -1,7 +1,25 @@
 #version 1.0.0.0
 #made by the challenger 346
-print("What virus are you scanning for?")
-a = input()
-if a == a: #checks to see if the input will equal the input
-    while 3 > 2: #makes the lines below repeat
-        print("The " + a + " virus has been found!")
+print("What are you scanning for?")
+print("[1] virus")
+print("[2] maleware")
+print("[3] ransomware")
+option = input()
+if option == '1':
+		print("What virus are you scanning for?")
+		virus = input()
+		if virus == virus:
+  			while 3 > 2:
+    					print("The " + virus + " virus has been found!")
+if option == '2':
+  print("What maleware are you scanning for?")
+  maleware = input()
+  if maleware == maleware:
+   	while 3 > 2:
+     		print("The " + maleware + " maleware has been found!")
+if option == '3':
+  print("What ransomware are you scanning for?")
+  ransomware = input()
+  if ransomware == ransomware:
+   	while 3 > 2:
+     		print("The " + ransomware + " ransomeware has been found!")
