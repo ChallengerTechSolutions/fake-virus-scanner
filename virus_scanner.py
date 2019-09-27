1ed1198c40400008828982d8d94925dddb65592b
thechallenger346 <45742653+thechallenger346@users.noreply.github.com>
2019-09-26 22:45:34 -0400

    Add files via upload

--- virus_scanner.py
+++ virus_scanner.py
@@ -1,25 +1,39 @@
 #version 1.0.0.1
 #made by the challenger 346
 print("What are you scanning for?")
-print("[1] virus")
+print("[1] virus") #an option
 print("[2] maleware")
 print("[3] ransomware")
+print("[4] adware")
+print("[5] spyware")
 option = input()
-if option == '1':
+if option == '1': #checks to see if the user chose option 1
 		print("What virus are you scanning for?")
 		virus = input()
 		if virus == virus:
-  			while 3 > 2:
+  			while 3 > 2: #makes "print("The " + virus + " virus has been found!")" repeat
     					print("The " + virus + " virus has been found!")
-if option == '2':
+if option == '2': #checks to see if the user chose option 2
   print("What maleware are you scanning for?")
-  maleware = input()
-  if maleware == maleware:
-   	while 3 > 2:
+  maleware = input() #checks to see what the user put
+  if maleware == maleware: #repeats what the user chose
+   	while 3 > 2: #makes "print("The " + maleware + " maleware has been found!")" repeat
      		print("The " + maleware + " maleware has been found!")
 if option == '3':
   print("What ransomware are you scanning for?")
   ransomware = input()
-  if ransomware == ransomware:
-   	while 3 > 2:
+  if ransomware == ransomware: #checks to see if the user chose option 3
+   	while 3 > 2: #makes "print("The " + ransomware + " ransomeware has been found!")" repeat
      		print("The " + ransomware + " ransomeware has been found!")
+if option == '4':
+  print("What adware are you scanning for?")
+  adware = input()
+  if adware == adware:
+   	while 3 > 2:
+     		print("The " + adware + " ransomeware has been found!")
+if option == '5':
+  print("What spyware are you scanning for?")
+  spyware = input()
+  if spyware == spyware:
+   	while 3 > 2:
+     		print("The " + spyware + " ransomeware has been found!")
